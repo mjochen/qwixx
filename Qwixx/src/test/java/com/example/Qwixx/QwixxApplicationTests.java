0039742463951c730c1cbfb4a307b1e91e4db84d
Jochen Mariën <jochen.marien@thomasmore.be>
2026-03-11 13:57:30 +0100

    ?

--- Qwixx/src/test/java/com/example/Qwixx/QwixxApplicationTests.java
+++ Qwixx/src/test/java/com/example/Qwixx/QwixxApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.Qwixx;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class QwixxApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 1);
 	}
 
 }
